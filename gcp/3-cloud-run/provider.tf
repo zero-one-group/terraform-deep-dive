b7275791acc5b6b2694c6e2bca7864459594f4f9
Doni Rubiagatra <doni@zero-one-group.com>
2022-02-17 19:51:43 +0700

    add cloud run

---
+++ gcp/3-cloud-run/provider.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.11.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  region = "asia-southeast2"
+  zone = "asia-southeast2"
+}
+
+
+
+
+
+
+
+
+
+
+
